e3269e2bbca08efe86df5c6216d4b5eb0ba0af5f
Simon Tucker <simon@brotherlogic@gmail>
2018-12-16 18:16:19 -0800

    Adds ability to pull from data collector for data sets. This closes #15.

--- dataviewer.go
+++ dataviewer.go
@@ -6,23 +6,50 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/brotherlogic/goserver"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
+	pbdc "github.com/brotherlogic/datacollector/proto"
 	pbg "github.com/brotherlogic/goserver/proto"
+	"github.com/brotherlogic/goserver/utils"
 )
+
+type collector interface {
+	GetDataSets(ctx context.Context, req *pbdc.GetDataSetsRequest) (*pbdc.GetDataSetsResponse, error)
+}
+
+type prodCollector struct{}
+
+func (p *prodCollector) GetDataSets(ctx context.Context, req *pbdc.GetDataSetsRequest) (*pbdc.GetDataSetsResponse, error) {
+	ip, port, err := utils.Resolve("datacollector")
+	if err != nil {
+		return &pbdc.GetDataSetsResponse{}, err
+	}
+
+	conn, err := grpc.Dial(ip+":"+strconv.Itoa(int(port)), grpc.WithInsecure())
+	defer conn.Close()
+	if err != nil {
+		return &pbdc.GetDataSetsResponse{}, err
+	}
+
+	client := pbdc.NewDataCollectorServiceClient(conn)
+	return client.GetDataSets(ctx, req)
+}
 
 //Server main server type
 type Server struct {
 	*goserver.GoServer
+	collector collector
 }
 
 // Init builds the server
 func Init() *Server {
 	s := &Server{
 		&goserver.GoServer{},
+		&prodCollector{},
 	}
 	return s
 }
@@ -52,7 +79,12 @@ func (s *Server) deliver(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, fmt.Sprintf("Error: %v", err))
 		return
 	}
-	props := properties{Names: []string{"test1", "test2"}}
+	sets, err := s.getDataSets(context.Background())
+	if err != nil {
+		fmt.Fprintf(w, fmt.Sprintf("Error: %v", err))
+		return
+	}
+	props := properties{Names: sets}
 	err = s.render(string(data), props, w)
 	if err != nil {
 		fmt.Fprintf(w, fmt.Sprintf("Error: %v", err))
